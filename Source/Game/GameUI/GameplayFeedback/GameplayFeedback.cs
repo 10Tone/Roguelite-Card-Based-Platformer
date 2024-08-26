7685dbb85c5c78917ee2e5707300f0fafc9d23f6
ruurt <rstaverman@protonmail.com>
2024-08-26 08:19:24 +0200

    Add GameplayFeedback.cs and extra font

---
+++ Source/Game/GameUI/GameplayFeedback/GameplayFeedback.cs
@@ -0,0 +1,24 @@
+using Godot;
+using System;
+using AutoLoads;
+using Game;
+
+public partial class GameplayFeedback : Control
+{
+	private GlobalEvents _globalEvents;
+	private GlobalVariables _globalVariables;
+
+	public override void _EnterTree()
+	{
+		_globalEvents = GetNode<GlobalEvents>("/root/GlobalEvents");
+		_globalVariables = GetNode<GlobalVariables>("/root/GlobalVariables");
+		
+		_globalEvents.GameStateEntered += OnGameStateEntered;
+	}
+
+	private void OnGameStateEntered(GameState gamestate)
+	{
+		
+	}
+}
+
