2e1318c9ca630284e7dbaa07db47d6e7e7700bc0
ruurt <rstaverman@protonmail.com>
2024-08-26 09:35:38 +0200

    Add gamestate messages to GameplayFeedback.cs

--- Source/Game/GameUI/GameplayFeedback/GameplayFeedback.cs
+++ Source/Game/GameUI/GameplayFeedback/GameplayFeedback.cs
@@ -5,8 +5,12 @@ using Game;
 
 public partial class GameplayFeedback : Control
 {
+    [Export()] private string _playMessage, _buildMessage, _deathMessage, _levelFinishedMessage, _stageFinishedMessage;
+    
 	private GlobalEvents _globalEvents;
 	private GlobalVariables _globalVariables;
+
+    private Label _mainTextLabel;
 
 	public override void _EnterTree()
 	{
@@ -16,24 +20,30 @@ public partial class GameplayFeedback : Control
 		_globalEvents.GameStateEntered += OnGameStateEntered;
 	}
 
-	private void OnGameStateEntered(GameState gamestate)
+    public override void _Ready()
+    {
+        _mainTextLabel = GetNode<Label>("MainTextLabel");
+        _mainTextLabel.Text = "";
+    }
+
+    private void OnGameStateEntered(GameState gamestate)
 {
     switch (gamestate)
     {
         case var _ when gamestate == _globalVariables.GameStates[GameModeState.PlayModeState]:
-
+            _mainTextLabel.Text = _playMessage;
             break;
         case var _ when gamestate == _globalVariables.GameStates[GameModeState.BuildModeState]:
-
+            _mainTextLabel.Text = _buildMessage;
             break;
         case var _ when gamestate == _globalVariables.GameStates[GameModeState.DeathModeState]:
-
+            _mainTextLabel.Text = _deathMessage;
             break;
         case var _ when gamestate == _globalVariables.GameStates[GameModeState.LevelFinishedModeState]:
-
+            _mainTextLabel.Text = _levelFinishedMessage;
             break;
         case var _ when gamestate == _globalVariables.GameStates[GameModeState.StageFinishedModeState]:
-
+            _mainTextLabel.Text = _stageFinishedMessage;
             break;
     }
 }
